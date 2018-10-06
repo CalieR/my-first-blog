b0f9e42f9ff9ee93598805d55cb6499eee076417
CalieR <calielrushton@gmail.com>
2018-10-06 13:20:55 +0100

    My Django Girls app, initial commit

---
+++ python_intro.py
@@ -0,0 +1,8 @@
+def hi(name):
+    print("Hi " + name + "!")
+
+
+girls = ["Rachel", "Monica", "Phoebe", "Ola", "Calie"]
+for name in girls:
+    hi(name)
+    print("Next girl")
---
+++ wibble.py
@@ -0,0 +1,6 @@
+# for i in range(10):
+#     print("wibble")
+# print("wobble")
+
+for i in range(1, 6):
+    print(i)
